aa88f4bdf2954f7b9c1524b10f065b704418f1bb
Jorge Gonzalez Albisu <jorge.gonzalez.albisu@gmail.com>
2025-06-20 11:19:14 -0300

    Remove outdated documentation comments from enterIframeBody command

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-/**
- * This method returns the body of a single iframe that matches the given selector and checking the visibility of it.
- * It will only work when there is ONE iframe on the page.
- * The function is a Parent Cypress command, so it can be used in a test like this:
- * @param {string} iFrameSelectorCSS - The selector for the iframe you want to get the body of.
- * @example cy.enterIframeBody("iframe").within(($iFrameBody)=>{
- * cy.get("selector within the iframe").should("be.visible");
- * })
- * @returns The body of the iframe as a chainable Cypress command to keep interacting with elements within. This should be used with a `within(($iFrame)=>{...}) command to continue with the desired behaviors.
- */
 Cypress.Commands.add("enterIframeBody", (iFrameSelectorCSS) => {
 	cy.log(`Entering iFrame ${iFrameSelectorCSS}`);
 	return cy
